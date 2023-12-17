1e5afc8590e3ed0e530d1fd84e2bf1b13b08efe1
PurpleBanana143 <154022234+PurpleBanana143@users.noreply.github.com>
2023-12-17 16:26:26 -0500

    Create creepy_majic_8.py
    A Majic 8-Ball Python 3 code that I made. CONTAINS EASTER EGG!

---
+++ creepy_majic_8.py
@@ -0,0 +1,50 @@
+# Importing the randomly generated message.
+import random
+# Enter user's name here.
+name = "PurpleBanana143 "
+# Yes or no question here.
+question = "Is this real? Are you real?"
+# Question printing.
+print(name + "asks:\n" + question)
+# Giving the randomly generated number to the user for the numbered answer.
+random_number = random.randint(1, 14)
+# print(random_number)
+# Assigning the number to an answer.
+if random_number == 1:
+  answer = "Yes - definitely."
+elif random_number == 2:
+  answer = "Reply hazy, try again."
+elif random_number == 3:
+  answer = "My sources say no."
+elif random_number == 4:
+  answer = "Without a doubt."
+elif random_number == 5:
+  answer = "It is decidedly so."
+elif random_number == 6:
+  answer = "Ask again later."
+elif random_number == 7:
+  answer = "Outlook not so good."
+elif random_number == 8:
+  answer = "Very doubtful."
+elif random_number == 9:
+  answer = "Better not tell you now."
+elif random_number == 10:
+  answer = "You know the answer to this question."
+elif random_number == 11:
+  answer = "This is beyond mine and your reckoning."
+elif random_number == 12:
+  answer = "Some questions are best unanswered."
+elif random_number == 13:
+  answer = "Ends can hold the brightest detail."
+elif random_number == 14:
+  answer = """I̸̡̹̳͇̋̋n̵͈̈́ ̶͍̫̱̝̀t̷̻̤͊̐͜ȇ̷̙͇͚̎̂͘m̸̮̪̰̘̾̏͂̏p̵̛͖̓̉o̸̬͕̖͈͐r̷͇͕͙̠̆̄̓͐é̵͇͘ ̸̧́͆́͗t̶̼̙̪͐r̸̛̗̘̘̈́͘̚i̶̛̞͈͕͂̀̑s̸̤̥͆̍ẗ̵̼́͝í̷͙̱̽t̷̢͖͉̤̃ĭ̴͍͚̥͇̈́̒̔a̷̜͇̚ė̸̢̼͖̃͌̾
+̸̟̊Ȋ̸͈̎̄̂n̶͓͇̺̺͐c̶̦̥̪̃̈́́ͅi̷̝̱̍̚p̶̧̼͌i̵̧̜̐̈́̏ť̷͂̚͜ ̴͈̮̑͆̂a̵̻̝͓̔n̸̨͙͚̂n̴̦̟̦͐̆̋͌ȕ̴̙̽̑s̷͖̰̪̀̓͝ ̷͕̱̩̏͗͘ḟ̵̩̞̗̳̏͠͝į̶͎̿̄n̵͉̈i̷̛̛͉̎̀s̵̡̝̙̍̅̋
+̷̟̰͆R̷̯͙͚͊͗͛͝e̵̼̠͕͝s̴̺̳̏p̴̡̡̙̈́̉̈̈o̵̹͜͝͝n̷̤̼̣͗́s̷̡̱̈́͋͑̐u̶̱̹̇̉ͅm̵͕̊͗̀͠ ̸̦͂͛͐͠t̷̘̠̙̔ṵ̶̇̂̈u̷͙̩̒̔m̵̲̣̖͋ ̶̩̬͐̃̉̃ę̷̳̜̿͑̋s̷̟̾̈̀̈́ṫ̴̛̜̻ ̵̖̞͙̓͛ȉ̶͎̣̓͗̕ḃ̶̡̠̰̌͂̑i̶̲̮͔̤͝͠.̷̮̜̭̗̈́̆
+̸̢̳̯̃͜E̷̥̯͇̾̂̏Ŗ̴̠͋̓̅͜Ŕ̵͍̼̬̥͛O̵͙̟̝̪̾R̷̪̔ ̴̧̮͑I̵̧͕̹̼͊͛N̷̛̘̯̮̏V̵͔̆̌ͅẼ̴̙̟̣͔̎͌N̵͉̿̾̈́Ť̵̪͚͖̗͐U̸̡̨͈̫̔͋́̚S̷̝̮͔͖͋͐.̵̨͙͙̍̑
+̵̗̜͇͌͗͘T̴͎͒̿́E̵̢̨̹̚R̷̯̀̒͗M̵̎̅́͌ͅI̸͕͔̊́͐̈Ņ̸̨͈́̃͌̋Ǫ̷̹̀ ̴̛̼͓̀̔ͅP̶͕̏R̴̟͕͑͊̾͜Ǒ̵͚̤͑̔̚Ḡ̶̱̘͇͈̏̌͗Ṙ̸̜͘͝ͅA̵͍̰͆M̷͕͈̜͈͗̇M̸̰͉̌̋͠ͅĄ̵͉͖̅́́͛ ̸͕̯̭͛̎͜P̸̯̜̟̹̾͊̿͝R̵̟̗̓̑͘͝ͅÆ̷͉̼̃̈́̈V̵̟̈͜Ị̷͋̓̑͋Ǘ̴͈M̴̡̛̼̜̫͗̽̏ ̶̡͙̊͝N̸͈̱̯̊Ụ̴̡͚̥͋̐N̴̜͕̪͗̃͆̕C̴̖̔͐͠.̵̛̛̯̞̩̀͜
+̴͖̬̉̈̐̓Ǐ̸̜̲̀̊͠N̴̹̭̅̐̓̿V̸̫̪̬̇̚E̴͚͑̉̕ͅN̴̢͑͌I̷̼͛̀͠T̷͍̱̲̔ ̴̦̠̦̯͛Ń̶̛̜͝O̴̱̖͓̒̓ͅB̶͕̝̰̔̽Ï̵̦̳̱̫͌̀͛Ś̴͒̐͜.̸̜̋̏̅"
+"""
+else:
+  answer = "Error."
+# Answer of Majic 8-Ball.
+print("Magic 8-Ball's answer: " + answer)
